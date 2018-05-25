5de513dc329ee99a5ff5678bbf555f03802a776b
Izaya Orihara <izaya.san@outlook.com>
2018-05-25 08:41:22 +0800

    Finish Exp3

---
+++ FreeSpaceTable.java
@@ -0,0 +1,100 @@
+package cn.edu.whu;
+
+import java.util.ArrayList;
+
+public class FreeSpaceTable {
+    ArrayList<Area> fsTable;
+    ArrayList<File> fileList;
+
+    public FreeSpaceTable() {
+        fsTable = new ArrayList<>();
+        fsTable.add(new Area(0, 24000));
+        fileList = new ArrayList<>();
+    }
+
+    public boolean createFile(File file) {
+        Area area;
+        area = allocSpace(file.fileArea.blocks);
+        if (area == null) {
+            return false;
+        } else {
+            file.fileArea = area;
+            fileList.add(file);
+            return true;
+        }
+    }
+
+    public Area allocSpace(int size) {
+        for (Area fsArea :
+                fsTable) {
+            if (fsArea.blocks > size) {
+                fsTable.remove(fsArea);
+                fsTable.add(new Area(fsArea.StartIndex + size, fsArea.blocks - size));
+                return new Area(fsArea.StartIndex, size);
+            } else if (fsArea.blocks == size) {
+                fsTable.remove(fsArea);
+                return new Area(fsArea.StartIndex, size);
+            }
+        }
+        return null;
+    }
+    public boolean freeSpace(String name) {
+        File file = null;
+        boolean flag = false;
+        for (int i = 0; i < fileList.size(); i++) {
+            if (fileList.get(i).name.equals(name)) {
+                file = fileList.get(i);
+                flag = true;
+            }
+        }
+        if(flag == false){
+            return false;
+        }
+        for (Area fsArea :
+                fsTable) {
+            if (file.fileArea.StartIndex == fsArea.StartIndex + fsArea.blocks) {
+                fsTable.remove(fsArea);
+                file.fileArea.StartIndex = fsArea.StartIndex;
+                file.fileArea.blocks += fsArea.blocks;
+            } else if (fsArea.StartIndex == file.fileArea.StartIndex + file.fileArea.blocks) {
+                fsTable.remove(fsArea);
+                file.fileArea.blocks += fsArea.blocks;
+            }
+        }
+        fsTable.add(file.fileArea);
+        fileList.remove(file);
+        return true;
+
+    }
+}
+
+class Area {
+    int StartIndex;
+    int blocks;
+
+    public Area(int startIndex, int blocks) {
+        StartIndex = startIndex;
+        this.blocks = blocks;
+    }
+
+    public void showDiskInfo (){
+        System.out.println("Physical Record\tTrack\tCylinder");
+        for (int i = 0; i < blocks; i++) {
+            System.out.println(((StartIndex + i) % 6) + "\t\t\t" + (((StartIndex + i) / 6)%20) + "\t\t\t\t" + (((StartIndex + i) / 6) / 20));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return StartIndex + "\t\t\t" + blocks;
+    }
+}
+class File  {
+    String name;
+    Area fileArea;
+
+    public File(int size, String name) {
+        fileArea = new Area(0, size);
+        this.name = name;
+    }
+}
---
+++ TestForExp3.java
@@ -0,0 +1,55 @@
+package cn.edu.whu;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TestForExp3 {
+    public static void main(String[] args) {
+        FreeSpaceTable fsTable = new FreeSpaceTable();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Enter X to jump out program.");
+            System.out.println("Enter 1 with file info to create a file." +
+                    "Enter 0 with process name to terminate a process");
+            try {
+                String processStr = reader.readLine();
+                String[] processInfo = processStr.split(" ");
+                String control = processInfo[0];
+                if (control.equals("X") || control.equals("x"))
+                    return;
+                else if(control.equals("1")) {
+                    String name = processInfo[1];
+                    Integer size = Integer.parseInt(processInfo[2]);
+                    File file = new File(size, name);
+                    if (fsTable.createFile(file)){
+                        file.fileArea.showDiskInfo();
+                        System.out.println("\tStarting Block\tBlocks");
+                        for (int i = 0; i < fsTable.fsTable.size(); i++) {
+                            System.out.println(i+ "\t\t" + fsTable.fsTable.get(i));
+                        }
+                    } else {
+                        System.out.println("Insufficient Space!");
+                    }
+                } else if (control.equals("0")){
+                    String name = processInfo[1];
+                    if (fsTable.freeSpace(name)) {
+                        System.out.println("Process " + name + " has been terminated.");
+                        System.out.println("\tStarting Block\tBlocks");
+                        for (int i = 0; i < fsTable.fsTable.size(); i++) {
+                            System.out.println(i+ "\t" + fsTable.fsTable.get(i));
+                        }
+                    } else {
+                        System.out.println("Please Enter Valid Process Name");
+                    }
+                } else {
+                    System.out.println("Format error, please try again.");
+                }
+
+
+            } catch (Exception e) {
+                System.out.println("Format error, please try again.");
+            }
+        }
+
+    }
+}
